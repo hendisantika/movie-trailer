55a9e5dc883c7247cec6fe69fd64185bb912a555
hendisantika <hendisantika@yahoo.co.id>
2025-10-29 05:42:21 +0700

    #1 | @hendisantika | feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/movietrailer/MovieTrailerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.movietrailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTrailerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MovieTrailerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/movietrailer/MovieTrailerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.movietrailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTrailerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
